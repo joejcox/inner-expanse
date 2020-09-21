7cc0d6ab15c9187f9ad77adc8da443ff9f0373b3
Joe Cox <joecox@live.co.uk>
2020-09-22 00:02:10 +0100

    destructured data passed in to cart item component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -3,16 +3,16 @@ import React from "react";
 
 import "./cart-item.styles.scss";
 
-const CartItem = ({ item }) => {
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
   return (
-    <div key={item.id} className="cart_preview__item">
+    <div className="cart_preview__item">
       <div className="cart_preview__item_image">
-        <img src={item.imageUrl} alt={item.name} />
+        <img src={imageUrl} alt={name} />
       </div>
       <div className="cart_preview__item_info">
-        <h4>{item.name}</h4>
-        <span>£{item.price}</span>
-        <p>Quantity: {item.quantity}</p>
+        <h4>{name}</h4>
+        <span>£{price}</span>
+        <p>Quantity: {quantity}</p>
       </div>
     </div>
   );
